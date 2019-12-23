157811abc969dc8c1c7349de5a7dbdf425c70d89
BenjaminVincent <ben.tom.vincent@gmail.com>
2019-12-23 07:28:24 +0000

    won't upload online

---
+++ range.js
@@ -0,0 +1,15 @@
+const range = function (start, end, step) {
+  let arr = [];
+  if (start === undefined || end === undefined || step === undefined || start >= end || step === 0) {
+    return arr;
+  } else {
+    for (let i = start; i < end; i += step) {
+      arr.push(i);
+    }
+    return arr;
+  }
+}
+
+console.log(range(0, 10, 0));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
